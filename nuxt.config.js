e7e7a6d8b19301100c6cc5a1178c8b963d5a1ff9
Olga Filipova <chudaol@gmail.com>
2019-02-11 18:56:32 +0100

    added meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
 const pkg = require('./package')
-
+const name = 'EdEra Technologies - we create software to support online education'
 
 module.exports = {
   mode: 'spa',
@@ -12,7 +12,18 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+      { name: 'robots', content: 'index,follow' },
+      { name: 'keywords', content: 'e-learning, gamification, personification, adaptive learning, new work, learning and development' },
+      { itemprop: 'name', content: 'EdEra Technologies - we create software to support online education' },
+      { itemprop: 'description', content: pkg.description },
+      { itemprop: 'image', content: '/header-2.jpg' },
+      { name: 'twitter:title', content: name },
+      { name: 'twitter:description', content: pkg.description },
+      { name: 'twitter:image', content: '/header-2.jpg' },
+      { name: 'og:title', content: name },
+      { name: 'og:description', content: pkg.description },
+      { name: 'og:image', content: '/header-2.jpg' }
     ],
     script: [
       { src: '/js/jquery.min.js' },
@@ -24,7 +35,8 @@ module.exports = {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Cambo|Poppins:400,600', type: 'text/css' },
-      { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css' }
+      { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css' },
+      { rel: 'image_src', href: '/header-2.jpg' }
     ]
   },
 
