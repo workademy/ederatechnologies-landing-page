68f2894c08237c8f0e539b9d5e83df8901800d73
Olga Filipova <chudaol@gmail.com>
2019-02-11 17:07:52 +0100

    finished

--- components/FooterComponent.vue
+++ components/FooterComponent.vue
@@ -4,22 +4,16 @@
       <div class="row">
         <div class="col-md-2 col-sm-3">
           <div class="info">
-            <h5 class="title">Company</h5>
+            <h5 class="title">EdEra Technologies</h5>
             <nav>
               <ul>
                 <li>
                   <a href="#">Home</a></li>
                 <li>
-                  <a href="#">Find offers</a>
+                  <a href="#whoweare">Who We Are</a>
                 </li>
                 <li>
-                  <a href="#">Discover Projects</a>
-                </li>
-                <li>
-                  <a href="#">Our Portfolio</a>
-                </li>
-                <li>
-                  <a href="#">About Us</a>
+                  <a href="#whatwedo">What We Do</a>
                 </li>
               </ul>
             </nav>
@@ -31,67 +25,67 @@
             <nav>
               <ul>
                 <li>
-                  <a href="#">Contact Us</a>
+                  <a href="mailto:olga.filipova@ed-era.com">Contact Us</a>
                 </li>
-                <li>
-                  <a href="#">How it works</a>
-                </li>
-                <li>
-                  <a href="#">Terms &amp; Conditions</a>
-                </li>
-                <li>
-                  <a href="#">Company Policy</a>
-                </li>
-                <li>
-                  <a href="#">Money Back</a>
-                </li>
+                <!--<li>-->
+                  <!--<a href="#">How it works</a>-->
+                <!--</li>-->
+                <!--<li>-->
+                  <!--<a href="#">Terms &amp; Conditions</a>-->
+                <!--</li>-->
+                <!--<li>-->
+                  <!--<a href="#">Company Policy</a>-->
+                <!--</li>-->
+                <!--<li>-->
+                  <!--<a href="#">Money Back</a>-->
+                <!--</li>-->
               </ul>
             </nav>
           </div>
         </div>
-        <div class="col-md-3 col-sm-3">
-          <div class="info">
-            <h5 class="title">Latest News</h5>
-            <nav>
-              <ul>
-                <li>
-                  <a href="#">
-                    <i class="fa fa-twitter"></i> <b>Get Shit Done</b> The best kit in the market is here, just give it a try and let us...
-                    <hr class="hr-small">
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <i class="fa fa-twitter"></i> We've just been featured on <b> Awwwards Website</b>! Thank you everybody for...
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
+        <!--<div class="col-md-3 col-sm-3">-->
+          <!--<div class="info">-->
+            <!--<h5 class="title">Latest News</h5>-->
+            <!--<nav>-->
+              <!--<ul>-->
+                <!--<li>-->
+                  <!--<a href="#">-->
+                    <!--<i class="fa fa-twitter"></i> <b>Get Shit Done</b> The best kit in the market is here, just give it a try and let us...-->
+                    <!--<hr class="hr-small">-->
+                  <!--</a>-->
+                <!--</li>-->
+                <!--<li>-->
+                  <!--<a href="#">-->
+                    <!--<i class="fa fa-twitter"></i> We've just been featured on <b> Awwwards Website</b>! Thank you everybody for...-->
+                  <!--</a>-->
+                <!--</li>-->
+              <!--</ul>-->
+            <!--</nav>-->
+          <!--</div>-->
+        <!--</div>-->
         <div class="col-md-2 col-md-offset-1 col-sm-3">
           <div class="info">
             <h5 class="title">Follow us on</h5>
             <nav>
               <ul>
                 <li>
-                  <a href="#" class="btn btn-social btn-facebook btn-simple">
+                  <a href="https://www.facebook.com/EdEraUa/" target="_blank" class="btn btn-social btn-facebook btn-simple">
                     <i class="fa fa-facebook-square"></i> Facebook
                   </a>
                 </li>
                 <li>
-                  <a href="#" class="btn btn-social btn-dribbble btn-simple">
-                    <i class="fa fa-dribbble"></i> Dribbble
+                  <a href="https://www.youtube.com/EderaEducation" target="_blank" class="btn btn-social btn-dribbble btn-simple">
+                    <i class="fa fa-youtube"></i> Youtube
                   </a>
                 </li>
                 <li>
-                  <a href="#" class="btn btn-social btn-twitter btn-simple">
+                  <a href="https://twitter.com/edera_ua" target="_blank" class="btn btn-social btn-twitter btn-simple">
                     <i class="fa fa-twitter"></i> Twitter
                   </a>
                 </li>
                 <li>
-                  <a href="#" class="btn btn-social btn-reddit btn-simple">
-                    <i class="fa fa-google-plus-square"></i> Google+
+                  <a href="https://www.instagram.com/edera_ua/" target="_blank" class="btn btn-social btn-reddit btn-simple">
+                    <i class="fa fa-instagram"></i> Instagram
                   </a>
                 </li>
               </ul>
@@ -101,8 +95,17 @@
       </div>
       <hr>
       <div class="copyright">
-        © <script> document.write(new Date().getFullYear()) </script> Creative Tim, made with love
+        © {{ footerDate }} EdEra Technologies &hearts;
       </div>
     </div>
   </footer>
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        footerDate: new Date().getFullYear()
+      }
+    }
+  }
+</script>
